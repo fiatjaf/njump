2268bf022fa052ca4af4a5db1f5858e0937762a3
fiatjaf <fiatjaf@gmail.com>
2023-12-02 11:47:15 -0300

    properly build tailwind on github actions.

--- render_embedjs.go
+++ render_embedjs.go
@@ -7,7 +7,6 @@ import (
 
 func renderEmbedjs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/javascript")
-
 	fileContent, _ := static.ReadFile("static/embed.js")
 	w.Write(fileContent)
 }
