50306e08e55349ac259a2c24b5d0117b1a3eec32
fiatjaf <fiatjaf@gmail.com>
2024-10-10 15:21:50 -0300

    stop failing with a 429 on too many concurrent requests, just wait and redirect.

--- http_middleware.go
+++ http_middleware.go
@@ -62,11 +62,6 @@ func queueMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		if count > 2 {
 			log.Debug().Str("path", r.URL.Path).Uint32("count", count).Int("qidx", qidx).Str("ip", actualIP(r)).
 				Msg("too many concurrent requests")
-
-			if count > 4 {
-				http.Error(w, "", http.StatusTooManyRequests)
-				return
-			}
 		}
 
 		// lock (or wait for the lock)
